c7949f44bb371f613e44fad25e24992501b68b56
dithnim <nimsaradineth63@gmail.com>
2024-06-28 23:45:10 +0530

    modified:   react/src/components/SuccessPopup.jsx 	modified:   react/src/index.css

--- react/src/components/SuccessPopup.jsx
+++ react/src/components/SuccessPopup.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 
 const SuccessPopup = (status) => {
+    const toggleSuccess = () => {
+        document.getElementById('success').classList.toggle('success-popup-close')
+    }
+
   return (
-    <div className="bg-neutral-950 flex p-3 rounded-2xl absolute bottom-3 left-3  items-center">
+    <div className="success-popup bg-neutral-900  flex p-3 rounded-2xl absolute bottom-0 left-3  items-center">
       <label htmlFor="">Task added successfully</label>
-      <i className="bx bx-x flex items-center text-2xl text-neutral-600 ms-4 cursor-pointer"></i>
+      <i className="bx bx-x flex items-center text-2xl text-neutral-600 ms-4 cursor-pointer rounded-full hover:bg-white hover:text-black h-6 w-6" onClick={toggleSuccess} id="success"></i>
     </div>
   );
 };
