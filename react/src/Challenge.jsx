725fc06b86eceb7bc4dbde5818bce7266fdd1561
dithnim <nimsaradineth63@gmail.com>
2024-07-01 23:05:45 +0530

    modified:   react/src/Challenge.jsx

--- react/src/Challenge.jsx
+++ react/src/Challenge.jsx
@@ -8,8 +8,8 @@ const challenge = () => {
         Welcome to a fun challenge
       </h1>
       <img src={photo} alt="photo" className="w-[640px] h-[427px] mt-16" />
-      <div className="bottom mt-5 flex items-center justify-between">
-        <h2>Something is not right with this image. Better check it out :)</h2>
+      <div className="bottom mt-20 flex items-center justify-between w-[60%]">
+        <h2>A clue is hidden in this image. Are you smart enough to find it :) (Hint :~ www.example.netlify.app)</h2>
         <a href={photo} download>
           <button className="ms-20 bg-white text-black p-2 rounded-full font-semibold cursor-pointer flex items-center"> Download Image</button>
         </a>
